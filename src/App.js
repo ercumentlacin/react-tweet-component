9429e20d2c81bf39dcad3da939765f20b72054b9
ercumentlacin <forever.ducale@gmail.com>
2020-12-24 14:00:58 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
 import "./style.css";
-import Card from "./components/Card";
-import Collapse from "./components/Collapse";
-import CardComponent from "./components/CardComponent";
 import Twitter from "./components/Twitter/Twitter";
 
 export default function App() {
-  return (
-    // Card
-    // <CardComponent />
-    <Twitter />
-  );
+  return <Twitter />;
 }
